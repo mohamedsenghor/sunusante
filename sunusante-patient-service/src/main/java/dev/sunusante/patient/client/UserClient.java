6cd8142d8aceff70818f1dbe106e38f36e11d2f2
mohamedsenghor <mohamedsenghor05@gmail.com>
2026-06-07 12:41:05 +0000

    Integrating Full Authentication Process through Admin and Open APIs Endpoint

---
+++ sunusante-patient-service/src/main/java/dev/sunusante/patient/client/UserClient.java
@@ -0,0 +1,13 @@
+package dev.sunusante.patient.client;
+
+import dev.sunusante.patient.config.FeignConfiguration;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.ResponseEntity;
+
+@FeignClient(name = "sunusanteGatewayService", configuration = FeignConfiguration.class)
+public interface UserClient {
+    @GetMapping("/api/users/{login}/exists")
+    Boolean checkUserExists(@PathVariable("login") String login);
+}
